91d2fe288785265694b2d4a167424a46550a3543
wutthiphat-pon <wutthiphat.pon@student.mahidol.edu>
2026-02-15 20:33:07 +0700

    BankSystem.java

---
+++ BankSystem.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class BankSystem {
+
+	private ArrayList<Customer> customers;
+	
+	
+	public BankSystem() {
+        customers = new ArrayList<>();
+    }
+	
+	public void register(String firstName, String lastName, String password, double balance) {}
+	
+	
+	
+	
+	
+	
+}
