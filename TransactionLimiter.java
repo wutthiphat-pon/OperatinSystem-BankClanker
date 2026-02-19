de4b5c1486342134e06cae705924043472274331
ploo <ploo34618@gmail.com>
2026-02-19 11:31:27 +0700

    Add TransactionLimiter class with transaction validation
    Implemented transaction limiter with max transactions and amount checks.

--- TransactionLimiter.java
+++ TransactionLimiter.java
@@ -1,13 +1,41 @@
 
 public class TransactionLimiter {
-	
-  //TODO Create a transactionlimiter
-	
-	
-	
-	
-	
-  //TODO boolean method of the limiter	
 
+    private int maxTransactions;   
+    private int currentCount;     
+    private double maxAmount;      
 
+    // Constructor
+    public TransactionLimiter(int maxTransactions, double maxAmount) {
+        this.maxTransactions = maxTransactions;
+        this.maxAmount = maxAmount;
+        this.currentCount = 0;
+    }
+
+    // Boolean method
+    public boolean isAllowed(double amount) {
+        if (currentCount >= maxTransactions) {
+            return false;
+        }
+
+        if (amount > maxAmount) {
+            return false;
+        }
+
+        return true;
+    }
+
+    // Transaction method
+    public void transaction(double amount) {
+        if (isAllowed(amount)) 
+        {
+            currentCount++;
+            System.out.println("Transaction success.");
+        } 
+        
+        else 
+        {
+            System.out.println("Transaction fail.");
+        }
+    }
 }
